eb64f3bfb79cf0188abeefbc4b2d56930eaa3abb
Rajiv <millord237@gmail.com>
2025-11-22 16:10:54 +0800

    update mongodb setup

--- docker/setup.sh
+++ docker/setup.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # Create data directories
-mkdir -p data/prometheus data/mongo
+mkdir -p data/prometheus data/mongo-container data/mongo-host
 
 # Change permissions
-chmod 777 data/prometheus data/mongo
+chmod 777 data/prometheus data/mongo-container data/mongo-host
